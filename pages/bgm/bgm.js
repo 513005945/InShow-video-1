1980b436c095320dd6c83a56ff06d5ebd6ed499a
Lytine <3038267475@qq.com>
2018-09-26 16:52:33 +0800

    修改了多个view绑定相同点击事件，如何绑定当前view的问题

--- pages/bgm/bgm.js
+++ pages/bgm/bgm.js
@@ -1,4 +1,9 @@
 var data = require('../../utils/data.js').songs;
+//获取应用实例
+var app = getApp()
+//背景音乐api
+const backgroundAudioManager = wx.getBackgroundAudioManager()
+
 Page({
   data: {
     /**轮播图 */
@@ -6,7 +11,9 @@ Page({
       'http://p3.music.126.net/bKFfzVVNmdLTaRN5uHHPqA==/18786255672743757.jpg',
       'http://p4.music.126.net/n15ddawhY4cyIzFu23CSJA==/1401877341861315.jpg',
       'http://p3.music.126.net/zMwH3zh33TAacyh2_4RjXw==/1375489062675977.jpg'
-    ]
+    ],
+    isPlayingMusic: true,
+
   },
   /**搜索框跳转 */
   sousuo: function(e) {
@@ -15,7 +22,7 @@ Page({
     })
   },
   /**音乐列表 */
-  onLoad: function() {
+  onLoad: function(option) {
     var rs = [],
       idsMap = {},
       keys = Object.keys(data),
@@ -42,12 +49,42 @@ Page({
     });
 
     wx.setStorageSync('ids', idsMap);
+
   },
-  playTap: function(e) {
+
+  //音乐播放
+  onMusicTap: function(e) {
+    console.log("啦啦啦啦打印这个模块的具体信息", e);
     const dataset = e.currentTarget.dataset;
-    wx.navigateTo({
-      //url: `../play/index?id=${dataset.id}`
-      url: `../templay/templay`
+    var id = dataset.id; //获取id
+    let data_recommends = this.data.recommends
+    // 遍历recommends的长度
+    for (var i = 0; i < data_recommends.length; i++) {
+      data_recommends[i].show = false;
+      // 如果当前的ID等于选择的ID，就添加一个SHOW属性
+      if (data_recommends[i].id == id) {
+        data_recommends[i].show = true
+      }
+    }
+    this.setData({
+      recommends: data_recommends
     })
-  }
+
+
+    var isPlayingMusic = this.data.isPlayingMusic;
+    if (isPlayingMusic) {
+      backgroundAudioManager.src = "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46",
+        this.setData({
+          isPlayingMusic: false
+        })
+    } else {
+      //wx.pauseBackgroundAudio(); //暂停播放音乐
+      wx.stopBackgroundAudio(); //停止播放音乐
+      this.setData({
+        isPlayingMusic: true
+      })
+    }
+  },
+
+
 })

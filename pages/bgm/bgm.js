fe2b1049b514f58f7791da2cf7b6b19b76866b31
Lytine <3038267475@qq.com>
2018-09-28 09:32:28 +0800

    暂存下---播放按钮一旦按下，再次按就关闭的状态，参考数组思路，纪念bug

--- pages/bgm/bgm.js
+++ pages/bgm/bgm.js
@@ -1,8 +1,8 @@
 var data = require('../../utils/data.js').songs;
-//获取应用实例
-var app = getApp()
-//背景音乐api
-const backgroundAudioManager = wx.getBackgroundAudioManager()
+
+//获取到audio的上下文对相关
+//audioContext 通过 audioId 跟一个 组件绑定，通过它可以操作对应的组件
+this.audioContext = wx.createAudioContext('audio');
 
 Page({
   data: {
@@ -12,7 +12,8 @@ Page({
       'http://p4.music.126.net/n15ddawhY4cyIzFu23CSJA==/1401877341861315.jpg',
       'http://p3.music.126.net/zMwH3zh33TAacyh2_4RjXw==/1375489062675977.jpg'
     ],
-    isPlayingMusic: true,
+    isPlayingMusic: false,
+
 
   },
   /**搜索框跳转 */
@@ -50,41 +51,54 @@ Page({
 
     wx.setStorageSync('ids', idsMap);
 
+
   },
 
-  //音乐播放
   onMusicTap: function(e) {
-    console.log("啦啦啦啦打印这个模块的具体信息", e);
+    var that = this;
+    console.log("打印这个模块的具体信息", e);
     const dataset = e.currentTarget.dataset;
-    var id = dataset.id; //获取id
-    let data_recommends = this.data.recommends
+    var id = dataset.id;
+    var index = dataset.index;
+    let data_recommends = this.data.recommends;
+    //每次都触发这个开关
+    data_recommends[index].show = !data_recommends[index].show;
+    console.log("是不是真的呢"+data_recommends[index].show)
     // 遍历recommends的长度
     for (var i = 0; i < data_recommends.length; i++) {
-      data_recommends[i].show = false;
+      //var showing = 'data_recommends' + '[' + i + ']' + '.' + 'show';
+      //data_recommends[i].show = false;
+
       // 如果当前的ID等于选择的ID，就添加一个SHOW属性
       if (data_recommends[i].id == id) {
-        data_recommends[i].show = true
+       // data_recommends[i].show = true;
+
+        backgroundAudioManager.src = "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46"
+        console.log("data_recommends[i].show关掉之前" + data_recommends[i].show);
+
+        wx.stopBackgroundAudio(); //正常播放的话就停止.不写这个的话，下一首歌的播放还是原来上一首的
+      } else {
+        //没有遍历到的歌曲列表显示播放状态为false
+        //data_recommends[i].show = false;
+        
       }
+      /*
+      wx.getBackgroundAudioPlayerState({
+        success: function(e) {
+          console.log("获取音乐播放状态" + res.status);
+          if (e.status === 2) {
+            that.setData({
+              showing = false
+            })
+          }
+        }
+      })*/
     }
-    this.setData({
+    that.setData({
       recommends: data_recommends
     })
-
-
-    var isPlayingMusic = this.data.isPlayingMusic;
-    if (isPlayingMusic) {
-      backgroundAudioManager.src = "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46",
-        this.setData({
-          isPlayingMusic: false
-        })
-    } else {
-      //wx.pauseBackgroundAudio(); //暂停播放音乐
-      wx.stopBackgroundAudio(); //停止播放音乐
-      this.setData({
-        isPlayingMusic: true
-      })
-    }
   },
+
 
 
 })
